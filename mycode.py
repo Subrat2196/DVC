654b9b863fa16fef6e847311d02068f0c16c495c
subrat bahuguna <bahuguna.subrat211996@gmail.com>
2025-06-07 12:31:28 +0530

    Initial commit before DVC initialisation , created mycode.py and Data Folder

---
+++ mycode.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+# This is the data that we have 
+dic = {
+    "Name": ["Aarav", "Isha", "Rohan", "Priya", "Neha"],
+    "City": ["Delhi", "Mumbai", "Bangalore", "Chennai", "Pune"],
+    "Salary": [75000, 82000, 91000, 68000, 77000]
+}
+
+
+df = pd.DataFrame(dic)
+
+# Here we will create a folder named data, that will have our csv file of the above data
+
+os.makedirs("Data",exist_ok=True)
+
+path = os.path.join("Data",'sample_dataset.csv')
+
+df.to_csv(path,index=False)
+
